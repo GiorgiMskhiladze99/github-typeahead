791bfca730070c56df59c3e108a5cb6e889273db
GiorgiMskhiladze99 <g.mskhiladze99@gmail.com>
2022-02-23 20:20:14 +0400

    added search filed, on every change fetch data from gitHub api, added loading animation and simple design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React, {useEffect, useState} from 'react';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
+import Search from "./components/search/search";
+import getUsers from "./helper/getUsers";
+import Users from "./components/users/users";
 
-    </div>
+import loadingImage from './assets/icons/loading.svg'
+
+function App() {
+
+  const [searchValue, setSearchValue] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(undefined);
+
+  useEffect(() => {
+
+    setIsLoading(false);
+
+    if (searchValue.length > 0){
+
+      setIsLoading(true);
+      setUserData(null);
+
+      //define data parameters
+      getUsers(searchValue, "best_match", "desc", 10, 1).then(res => setUserData(res));
+    }
+  }, [searchValue]);
+
+  const onChangeHandler = (inputValue) => {
+    setSearchValue(inputValue);
+  };
+
+  return (
+      <div className="App">
+
+        <Search onChangeHandler={onChangeHandler}/>
+        { searchValue && userData !== null && <Users data={userData}/>}
+
+        {/*it can be another component or maybe ported above the root element*/}
+        { isLoading && userData === null ? <div className={"loading__container"}>
+          <img className={"loading"} src={loadingImage} alt="loading"/>
+        </div> : ""}
+
+      </div>
   );
 }
 
