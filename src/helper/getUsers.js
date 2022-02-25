9937e33ec041bb89ec35c0d563e9cbf91657181f
GiorgiMskhiladze99 <g.mskhiladze99@gmail.com>
2022-02-25 13:25:38 +0400

    fixed api address

--- src/helper/getUsers.js
+++ src/helper/getUsers.js
@@ -1,7 +1,7 @@
 const API_URL = "https://api.github.com/search/users";
 
 const getUsers = (keyword, sort, ordering, amount, page_amount) => {
-    return fetch(`${API_URL}?q=${keyword}&sort=${sort}&order=${ordering}&per_page=${amount}&page=${page_amount}`)
+    return fetch(`${API_URL}?q=${keyword}+in:login&sort=${sort}&order=${ordering}&per_page=${amount}&page=${page_amount}`)
         .then(response => response.json())
         .then( data => data)
         .catch( error => console.error(error));
